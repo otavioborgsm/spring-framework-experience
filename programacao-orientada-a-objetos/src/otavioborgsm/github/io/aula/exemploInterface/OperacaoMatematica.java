99c7174b30fab9dd36b8abfaf8e4d7ca4138f8e0
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-07-15 00:11:25 -0300

    exemplo de interface

---
+++ programacao-orientada-a-objetos/src/otavioborgsm/github/io/aula/exemploInterface/OperacaoMatematica.java
@@ -0,0 +1,13 @@
+package otavioborgsm.github.io.aula.exemploInterface;
+
+public interface OperacaoMatematica {
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+
+}
